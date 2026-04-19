f4f57bf20e524629c429e9a43ca7cd310a15e03a
TommasoFachin <69682170+TommasoFachin@users.noreply.github.com>
2026-04-19 11:12:04 +0200

    aggiunto memoria al both

--- progetto-langchain/es1.py
+++ progetto-langchain/es1.py
@@ -1,5 +1,5 @@
 from langchain_openai import AzureChatOpenAI
-from langchain_core.messages import HumanMessage
+from langchain_core.messages import HumanMessage, SystemMessage
 from dotenv import load_dotenv
 import os
 
@@ -17,16 +17,26 @@ chat = AzureChatOpenAI(
     api_version=api_version,
     azure_endpoint=azure_endpoint,
     api_key=api_key,
-    temperature=0.4, # Imposta la creatività del modello (0.0 = preciso, 1.0+ = creativo)
+    temperature=0.2, # Imposta la creatività del modello (0.0 = preciso, 1.0+ = creativo)
 )
 
+#chiede la lingua preferita all'utente
+user_lang = input("in quale lingua deve parlare il both ? ").strip().lower()
+system_prompt = SystemMessage(content=f"rispondi sempre in {user_lang} e non in altre lingue.")
 
 
 # Crea una conversazione con un messaggio umano
 def chat_loop():
     print("Digita 'exit' per uscire dalla chat.")
     #saluto iniziale del both
-    saluto = chat.invoke([HumanMessage(content="Saluta l'utente in modo amichevole e chiedi come posso aiutarlo oggi.")])
+    
+    # Lista per mantenere la cronologia della conversazione
+    chat_history = [system_prompt]
+    
+    saluto_request = HumanMessage(content="Saluta l'utente in modo amichevole e chiedi come posso aiutarlo oggi.")
+    chat_history.append(saluto_request)
+    saluto = chat.invoke(chat_history)
+    chat_history.append(saluto)
     print("BOTH:", saluto.content + "\n")
     while True:
         user_input = input("TU: ")
@@ -34,7 +44,9 @@ def chat_loop():
             print("Uscita dalla chat.")
             break
         try:
-            response = chat.invoke([HumanMessage(content=user_input)])
+            chat_history.append(HumanMessage(content=user_input))   #memoorizza la domanda dell'utente per il contesto futuro
+            response = chat.invoke(chat_history)
+            chat_history.append(response) # Memorizza la risposta per il contesto futuro
             print("BOTH:", response.content + "\n")
         except Exception as e: # Gestione più generica degli errori
             print(f"\nSi è verificato un errore: {e}")
