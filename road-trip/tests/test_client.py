6df5487b3fbe1ec1d3d007e2842ff171f9a0ec67
TommasoFachin <69682170+TommasoFachin@users.noreply.github.com>
2026-04-28 16:05:13 +0200

    aggiunto file test_client.py per esecuzione

---
+++ road-trip/tests/test_client.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+URL = "http://127.0.0.1:8000/genera-itinerario"
+
+# Carica il JSON della richiesta
+with open("richiesta_viaggio.json", "r", encoding="utf-8") as f:
+    payload = json.load(f)
+
+# Invia la richiesta POST
+response = requests.post(URL, json=payload)
+
+# Stampa il risultato
+print("STATUS:", response.status_code)
+print("RISPOSTA:")
+print(json.dumps(response.json(), indent=4, ensure_ascii=False))
